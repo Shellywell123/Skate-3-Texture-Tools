d3b5889ce4e3d612cbe716f9bd1b06a019ef3349
Shellywell123 <benjamin@shellswell.com>
2021-02-27 09:51:04 +0000

    began noesis cmdline bat file

---
+++ src/batch_files/Convert_PSGs_to_DDSs.bat
@@ -0,0 +1,5 @@
+:: will be a batch file for exporting all psg files in sub direcories to dds files in the same file structure using noesis cmd mode
+
+:: currently only launches Noesis
+set Noesis="C:\Users\benja\Documents\Tools\Skate Texture Mod\Noesis\Noesis.exe"
+%Noesis%
